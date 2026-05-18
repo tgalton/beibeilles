6dace9b55b18da6b88774f7dd5f12a56a300e02a
tgalton <galtontom@gmail.com>
2026-05-19 00:09:38 +0200

    still debugging sensor

--- api/app/routers/sensor_device.py
+++ api/app/routers/sensor_device.py
@@ -1,12 +1,8 @@
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
 
-from fastapi import HTTPException
 from fastapi import APIRouter
 from fastapi import Depends
 
-from app.models.sensor_device import SensorDevice
-from app.repositories import sensor_device_repository
 from app.database import get_db
 
 from app.schemas.sensor_device import SensorDeviceCreate
@@ -20,7 +16,7 @@ router = APIRouter(
     tags=["SensorDevices"],
 )
 
-# Create
+
 @router.post(
     "",
     response_model=SensorDeviceRead,
@@ -29,30 +25,12 @@ def create_sensor_device(
     sensor_device: SensorDeviceCreate,
     db: Session = Depends(get_db),
 ):
-    db_sensor_device = SensorDevice(
-        name=sensor_device.name,
-        serial_number=sensor_device.serial_number,
-        hive_id=sensor_device.hive_id,
+    return sensor_device_service.create_sensor_device(
+        db=db,
+        sensor_device=sensor_device,
     )
 
-    try:
 
-        return sensor_device_repository.create(
-            db=db,
-            sensor_device=db_sensor_device,
-        )
-
-    except IntegrityError:
-
-        raise HTTPException(
-            status_code=400,
-            detail=(
-                "Sensor device already exists "
-                "or hive_id is invalid"
-            ),
-        )
-    
-# GetAllSensors
 @router.get(
     "",
     response_model=list[SensorDeviceRead],
@@ -60,17 +38,20 @@ def create_sensor_device(
 def get_all_sensor(
     db: Session = Depends(get_db),
 ):
-    return sensor_device_service.get_sensor_all_device(db=db)
+    return sensor_device_service.get_sensor_all_device(
+        db=db,
+    )
 
-# GetBySerial
+
 @router.get(
     "/{serial_number}",
-    response_model= SensorDeviceRead)
+    response_model=SensorDeviceRead,
+)
 def get_sensor(
     serial_number: str,
-    db : Session = Depends(get_db),
+    db: Session = Depends(get_db),
 ):
-    return sensor_device_service.get_sensor_by_serial(
-        db = db,
-        serial_number = serial_number,
+    return sensor_device_service.get_sensor(
+        db=db,
+        serial_number=serial_number,
     )
