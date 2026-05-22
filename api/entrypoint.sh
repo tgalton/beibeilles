8be171278b31e24b6de72631e182131ef382a76f
tgalton <galtontom@gmail.com>
2026-05-22 22:56:14 +0200

    entrypoint for alembic server

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+set -e
+
 echo "Running database migrations..."
 
 alembic upgrade head
 
 echo "Starting API..."
 
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
