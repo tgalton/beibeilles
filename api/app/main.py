8833b8f65c3ba7a362cf56255b549c774db00008
tgalton <galtontom@gmail.com>
2026-05-25 17:17:43 +0200

    forget import

--- api/app/main.py
+++ api/app/main.py
@@ -10,8 +10,11 @@ from app.routers.hive_level import router as hive_level_router
 from app.routers.sensor_device import (
     router as sensor_device_router,
 )
-from app.routers.measurement import (
-    router as measurement_router,
+from app.routers.measurement_raw import (
+    router as measurement_raw_router,
+)
+from app.routers.measurement_5m import (
+    router as measurement_5m_router,
 )
 
 @asynccontextmanager
@@ -40,7 +43,9 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(hive_router)
 app.include_router(hive_level_router)
 app.include_router(sensor_device_router)
-app.include_router(measurement_router)
+app.include_router(measurement_raw_router)
+app.include_router(measurement_5m_router)
+
 
 
 # =========================================================
